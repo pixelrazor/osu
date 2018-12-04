82832bf9b45710422471a3615eca3c584b79a6d2
Unknown <darkria45@gmail.com>
2018-12-04 09:01:40 -0600

    Added User and Scores endpoint

--- endpoints.go
+++ endpoints.go
@@ -5,40 +5,74 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"regexp"
 	"time"
 )
 
-// WithMode specifies which mode to confine results to in searches
-func WithMode(mode Mode) Option {
+// BeatmapsIncludeConverted includes converted beatmaps (only valid when mode is not Osu)
+func BeatmapsIncludeConverted() BeatmapOption {
+	return func(s string) string {
+		return s + fmt.Sprint("&a=1")
+	}
+}
+
+// BeatmapsLimit specifies how many beatmaps to return (default and maximum value is 500)
+func BeatmapsLimit(limit int) BeatmapOption {
+	if limit < 1 {
+		limit = 1
+	} else if limit > 500 {
+		limit = 500
+	}
+	return func(s string) string {
+		return s + fmt.Sprintf("&limit=%d", limit)
+	}
+}
+
+// BeatmapsWithMode specifies which mode to confine results to
+func BeatmapsWithMode(mode Mode) BeatmapOption {
 	return func(s string) string {
 		return s + fmt.Sprintf("&m=%d", mode)
 	}
 }
 
-// WithID confines the search to beatmaps with a specific BeatmapID
-func WithID(ID string) Option {
+// BeatmapsWithHash confines the search to beatmaps with a specific BeatmapID
+func BeatmapsWithHash(ID string) BeatmapOption {
 	return func(s string) string {
-		return s + fmt.Sprintf("&b=%v", ID)
+		return s + fmt.Sprintf("&h=%v", url.QueryEscape(ID))
 	}
 }
 
-// WithSetID confines the search to beatmaps with a specific BeatmapsetID
-func WithSetID(ID string) Option {
+// BeatmapsWithID confines the search to beatmaps with a specific BeatmapID
+func BeatmapsWithID(ID string) BeatmapOption {
 	return func(s string) string {
-		return s + fmt.Sprintf("&s=%v", ID)
+		return s + fmt.Sprintf("&b=%v", url.QueryEscape(ID))
 	}
 }
 
-// Since confines the search to beatmaps ranked or loved since date
-func Since(date time.Time) Option {
+// BeatmapsWithSetID confines the search to beatmaps with a specific BeatmapsetID
+func BeatmapsWithSetID(ID string) BeatmapOption {
+	return func(s string) string {
+		return s + fmt.Sprintf("&s=%v", url.QueryEscape(ID))
+	}
+}
+
+// BeatmapsByCreator confines the search to beatmaps created by a specific user
+func BeatmapsByCreator(ID string, IDType UsernameType) BeatmapOption {
+	return func(s string) string {
+		return s + fmt.Sprintf("&u=%v&type=%v", url.QueryEscape(ID), url.QueryEscape(string(IDType)))
+	}
+}
+
+// BeatmapsSince confines the search to beatmaps ranked or loved since date
+func BeatmapsSince(date time.Time) BeatmapOption {
 	return func(s string) string {
 		return s + fmt.Sprintf("&since=%v", date.Format("2006-01-02"))
 	}
 }
 
 // Beatmaps fetches a list of beatmaps
-func (client *Client) Beatmaps(opts ...Option) ([]*Beatmap, error) {
+func (client *Client) Beatmaps(opts ...BeatmapOption) ([]*Beatmap, error) {
 	query := apiURL + "get_beatmaps?k=" + client.key
 	for _, opt := range opts {
 		query = opt(query)
@@ -60,4 +94,115 @@ func (client *Client) Beatmaps(opts ...Option) ([]*Beatmap, error) {
 	maps := make([]*Beatmap, 0)
 	err = json.Unmarshal(body, &maps)
 	return maps, err
+}
+
+// UserMode specifies which mode to show info for in the User struct (default is Osu)
+func UserMode(mode Mode) UserOption {
+	return func(s string) string {
+		return s + fmt.Sprintf("&m=%d", mode)
+	}
+}
+
+// UserEventsSince specifies the max number of days between now and last event date. Range of 1-31 (default is 1)
+func UserEventsSince(days int) UserOption {
+	if days < 1 {
+		days = 1
+	} else if days > 31 {
+		days = 31
+	}
+	return func(s string) string {
+		return s + fmt.Sprintf("&event_days=%d", days)
+	}
+}
+
+// User fetches information for a specific user
+func (client *Client) User(ID string, IDType UsernameType, opts ...UserOption) (*User, error) {
+	query := apiURL + "get_user?k=" + client.key
+	query = BeatmapsByCreator(ID, IDType)(query)
+	for _, opt := range opts {
+		query = opt(query)
+	}
+	resp, err := client.c.Get(query)
+	if err != nil {
+		return nil, errors.New("osu.Client.User: " + err.Error())
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, errors.New("osu.Client.User: " + resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("osu.Client.Beatmaps: " + err.Error())
+	}
+	var regx = regexp.MustCompile(`(date"[[:space:]]*:[[:space:]]*"[0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})"`)
+	body = regx.ReplaceAll(body, []byte(`${1}T${2}-00:00"`))
+	user := make([]*User, 0)
+	err = json.Unmarshal(body, &user)
+	if len(user) == 1 {
+		return user[0], err
+	}
+	return nil, err
+}
+
+// ScoresWithMode confines results to those with the specified mode
+func ScoresWithMode(m Mode) ScoresOption {
+	return func(s string) string {
+		return s + fmt.Sprintf("&m=%d", m)
+	}
+}
+
+// ScoresByUser confines results to just scores from the specific user
+func ScoresByUser(ID string, IDType UsernameType) ScoresOption {
+	return func(s string) string {
+		return s + fmt.Sprintf("&u=%v&type=%v", url.QueryEscape(ID), url.QueryEscape(string(IDType)))
+	}
+}
+
+// ScoresWithMods confines results to those that have the given mods
+func ScoresWithMods(mods ...Mod) ScoresOption {
+	m := 0
+	for _, v := range mods {
+		m |= int(v)
+	}
+	return func(s string) string {
+		return s + fmt.Sprintf("&mods=%d", m)
+	}
+}
+
+// ScoresLimit specifies how many beatmaps to return (default 50, max 100)
+func ScoresLimit(limit int) ScoresOption {
+	if limit < 1 {
+		limit = 1
+	} else if limit > 100 {
+		limit = 100
+	}
+	return func(s string) string {
+		return s + fmt.Sprintf("&limit=%d", limit)
+	}
+}
+
+// Scores fetches a list of Scores for a specific beatmap
+func (client *Client) Scores(ID string, opts ...ScoresOption) ([]*Score, error) {
+	query := apiURL + "get_scores?k=" + client.key
+	query = BeatmapsWithID(ID)(query)
+	for _, opt := range opts {
+		query = opt(query)
+	}
+	resp, err := client.c.Get(query)
+	if err != nil {
+		return nil, errors.New("osu.Client.Scores: " + err.Error())
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return nil, errors.New("osu.Client.Scores: " + resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("osu.Client.Scores: " + err.Error())
+	}
+	var regx = regexp.MustCompile(`(date"[[:space:]]*:[[:space:]]*"[0-9]{4}-[0-9]{2}-[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})"`)
+	body = regx.ReplaceAll(body, []byte(`${1}T${2}-00:00"`))
+	scores := make([]*Score, 0)
+	err = json.Unmarshal(body, &scores)
+	return scores, err
 }
--- structs.go
+++ structs.go
@@ -47,6 +47,107 @@ const (
 	ScoreIncreaseMods = Hidden | HardRock | DoubleTime | Flashlight | FadeIn
 )
 
+func (m Mod) String() string {
+	switch m {
+	case None:
+		return "None"
+	case NoFail:
+		return "NoFail"
+	case Easy:
+		return "Easy"
+	case TouchDevice:
+		return "TouchDevice"
+	case Hidden:
+		return "Hidden"
+	case HardRock:
+		return "HardRock"
+	case SuddenDeath:
+		return "SuddenDeath"
+	case DoubleTime:
+		return "DoubleTime"
+	case Relax:
+		return "Relax"
+	case HalfTime:
+		return "HalfTime"
+	case Nightcore:
+		return "Nightcore"
+	case Flashlight:
+		return "Flashlight"
+	case Autoplay:
+		return "Autoplay"
+	case SpunOut:
+		return "SpunOut"
+	case Relax2:
+		return "Relax2"
+	case Perfect:
+		return "Perfect"
+	case Key4:
+		return "Key4"
+	case Key5:
+		return "Key5"
+	case Key6:
+		return "Key6"
+	case Key7:
+		return "Key7"
+	case Key8:
+		return "Key8"
+	case FadeIn:
+		return "FadeIn"
+	case Random:
+		return "Random"
+	case Cinema:
+		return "Cinema"
+	case Target:
+		return "Target"
+	case Key9:
+		return "Key9"
+	case KeyCoop:
+		return "KeyCoop"
+	case Key1:
+		return "Key1"
+	case Key3:
+		return "Key3"
+	case Key2:
+		return "Key2"
+	case ScoreV2:
+		return "ScoreV2"
+	case LastMod:
+		return "LastMod"
+	default:
+		return "Invalid Mod"
+	}
+}
+
+// Mods represents a list of mods
+type Mods int
+
+func (mods Mods) String() string {
+	l := mods.List()
+	if len(l) == 0 {
+		return "[]"
+	}
+	ret := ""
+	for _, v := range l {
+		ret += ", " + v.String()
+	}
+	return "[" + ret[2:] + "]"
+}
+
+// List returns a slice containing the individual mods contained by a field
+func (mods Mods) List() []Mod {
+	num := int(mods)
+	list := make([]Mod, 0)
+	i := uint(0)
+	for num != 0 {
+		if num&1 == 1 {
+			list = append(list, Mod(1<<i))
+		}
+		i++
+		num >>= 1
+	}
+	return list
+}
+
 // Possible values of Beatmap.Approved
 const (
 	Loved     = 4
@@ -63,7 +164,9 @@ type UsernameType string
 
 // All username types
 const (
-	UserID   UsernameType = "int"
+	// UserID is for referring to the ID number of a user
+	UserID UsernameType = "int"
+	// Username is for referring to the name of a user
 	Username UsernameType = "string"
 )
 
@@ -141,14 +244,76 @@ type Beatmap struct {
 	FileMd5          string    `json:"file_md5"`
 	Mode             Mode      `json:"mode,string"`
 	Tags             string    `json:"tags"`
-	FavouriteCount   int       `json:"favourite_count,string"`
-	Playcount        int       `json:"playcount,string"`
-	Passcount        int       `json:"passcount,string"`
-	MaxCombo         int       `json:"max_combo,string"`
+	FavouriteCount   int64     `json:"favourite_count,string"`
+	Playcount        int64     `json:"playcount,string"`
+	Passcount        int64     `json:"passcount,string"`
+	MaxCombo         int64     `json:"max_combo,string"`
 }
 
-// Option is used to add optional queries to the API calls
-type Option func(string) string
+// User holds all information relating to a user
+type User struct {
+	UserID             string       `json:"user_id"`
+	Username           string       `json:"username"`
+	JoinDate           time.Time    `json:"join_date,string"`
+	Count300           int64        `json:"count300,string"`
+	Count100           int64        `json:"count100,string"`
+	Count50            int64        `json:"count50,string"`
+	Playcount          int64        `json:"playcount,string"`
+	RankedScore        int64        `json:"ranked_score,string"`
+	TotalScore         int64        `json:"total_score,string"`
+	PpRank             int64        `json:"pp_rank,string"`
+	Level              float64      `json:"level,string"`
+	PpRaw              float64      `json:"pp_raw,string"`
+	Accuracy           float64      `json:"accuracy,string"`
+	CountRankSs        int64        `json:"count_rank_ss,string"`
+	CountRankSSH       int64        `json:"count_rank_ssh,string"`
+	CountRankS         int64        `json:"count_rank_s,string"`
+	CountRankSh        int64        `json:"count_rank_sh,string"`
+	CountRankA         int64        `json:"count_rank_a,string"`
+	Country            string       `json:"country"`
+	TotalSecondsPlayed int64        `json:"total_seconds_played,string"`
+	PpCountryRank      int64        `json:"pp_country_rank,string"`
+	Events             []*UserEvent `json:"events"`
+}
+
+// UserEvent holds information about recent events for a user
+type UserEvent struct {
+	DisplayHTML  string    `json:"display_html"`
+	BeatmapID    string    `json:"beatmap_id"`
+	BeatmapsetID string    `json:"beatmapset_id"`
+	Date         time.Time `json:"date,string"`
+	Epicfactor   int64     `json:"epicfactor,string"`
+}
+
+// Score holds iformation about a score for a specific beatmap
+type Score struct {
+	ScoreID         string    `json:"score_id"`
+	Score           int64     `json:"score,string"`
+	Username        string    `json:"username"`
+	Count300        int64     `json:"count300,string"`
+	Count100        int64     `json:"count100,string"`
+	Count50         int64     `json:"count50,string"`
+	Countmiss       int64     `json:"countmiss,string"`
+	Maxcombo        int64     `json:"maxcombo,string"`
+	Countkatu       int64     `json:"countkatu,string"`
+	Countgeki       int64     `json:"countgeki,string"`
+	Perfect         string    `json:"perfect"`
+	EnabledMods     Mods      `json:"enabled_mods,string"`
+	UserID          string    `json:"user_id"`
+	Date            time.Time `json:"date,string"`
+	Rank            string    `json:"rank"`
+	Pp              float64   `json:"pp,string"`
+	ReplayAvailable string    `json:"replay_available"`
+}
+
+// BeatmapOption is used to add optional queries to Client.Beatmaps
+type BeatmapOption func(string) string
+
+// UserOption is used to add optional queries to Client.User
+type UserOption func(string) string
+
+// ScoresOption is used to add optional queries to Client.Scores
+type ScoresOption func(string) string
 
 // Client executes requests to the endpoints
 type Client struct {
